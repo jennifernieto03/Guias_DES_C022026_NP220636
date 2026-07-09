5e79dd72795ee7a11818e19c55e8ff53693fccb4
jennifernieto03 <jennifernieto0311@gmail.com>
2026-07-09 16:50:09 -0600

    Agregar archivos de proyecto.

---
+++ Persona/Controllers/PersonaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persona.Models; 
+
+namespace Persona.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Instanciamos la clase Persona con datos simulados
+            var miPersona = new Models.Persona
+            {
+                DUI = "12345678-9",
+                Nombre = "Juan",
+                Apellido = "Pérez",
+                FechaNacimiento = new DateTime(1995, 8, 20)
+            };
+
+            // Pasamos el objeto al modelo de la vista
+            return View(miPersona);
+        }
+    }
+}
---
+++ Persona/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace Persona.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
