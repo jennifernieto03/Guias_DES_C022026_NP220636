2efac81ebcc2881b236c2e6e9131a444c05ff2e6
jennifernieto03 <jennifernieto0311@gmail.com>
2026-07-09 16:12:07 -0600

    Agregar archivos de proyecto.

---
+++ MVCPeliculas/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*public string Index()
+        {
+            return "Esta es mi acción <b> predeterminada </b>";
+        }*/
+
+        public ActionResult Welcome(string nombre, int numVeces = 1)
+        {
+            ViewData["nombre"] = "Hola " + nombre;
+            ViewData["numVeces"] = numVeces;
+            return View();
+        }
+
+        public string Greeting(string nombre, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {nombre}, ID: {id}");
+        }
+    }
+}
