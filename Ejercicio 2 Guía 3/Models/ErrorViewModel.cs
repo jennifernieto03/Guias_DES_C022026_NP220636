2882c6a8ae2d07623da35f7b6121717a7f8b6f57
jennifernieto03 <jennifernieto0311@gmail.com>
2026-07-16 21:54:31 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 Guía 3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_2_Guía_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
