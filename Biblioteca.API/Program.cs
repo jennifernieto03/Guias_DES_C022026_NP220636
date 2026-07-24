a990b8413c01a262913d76f0c8191b63ffe59def
jennifernieto03 <jennifernieto0311@gmail.com>
2026-07-23 18:00:21 -0600

    Guia 5 pt1

--- Biblioteca.API/Program.cs
+++ Biblioteca.API/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Swagger UI
+builder.Services.AddSwaggerGen(); // NUEVO
 
 var app = builder.Build();
 
@@ -12,6 +14,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    // Interfaz Swagger UI
+    app.UseSwagger(); // NUEVO
+    app.UseSwaggerUI(); // NUEVO
 }
 
 app.UseHttpsRedirection();
---
+++ Biblioteca.Common/Biblioteca.Entites.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Common
+{
+    public class AppSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
