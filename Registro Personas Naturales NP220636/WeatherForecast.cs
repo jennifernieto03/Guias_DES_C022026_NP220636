3b5ad63dc4751ceed909663b760303b3a98152e0
jennifernieto03 <jennifernieto0311@gmail.com>
2026-09-17 14:26:14 -0600

    Agregar archivos de proyecto.

---
+++ Registro Personas Naturales NP220636/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Registro_Personas_Naturales_NP220636
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
